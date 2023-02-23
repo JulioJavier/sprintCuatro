457803e851401f0ff16ad32a8dcc0b59c125b944
JulioJavier <01juliomtz@gmail.com>
2023-02-23 03:00:27 -0500

    feat: update styles page OrderDetails

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -4,55 +4,49 @@ import Navbar from "../layout/Navbar";
 
 const OrderDetails = () => {
   const { dishToDetails } = useInfo();
-  console.log(dishToDetails);
+  // console.log(dishToDetails);
 
   return (
-    <div className="container mx-auto px-4 sm:px-8">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl mb-4 sm:mb-6 md:mb-8">
-        Orders
-      </h1>
+    <div className="container mx-auto p-4 justify-center items-center">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl mb-4 sm:mb-6">Orders</h1>
       {dishToDetails.dishes.map((dish, index) => {
         return (
           <div
             key={index}
             className="flex sm:flex-row items-center justify-between mb-4 sm:mb-6 md:mb-8"
           >
-            <div className="flex gap-2">
-              <p className="text-base sm:text-lg md:text-xl text-slate-400">
+            <div className="flex gap-2 ">
+              <p className="text-base sm:text-lg  text-slate-400">
                 {dish.quantity} x
               </p>
-              <p className="text-base sm:text-lg md:text-xl">{dish.name}</p>
+              <p className="text-base sm:text-lg ">{dish.name}</p>
             </div>
             <div>
-              <p className="text-base sm:text-lg md:text-xl">${dish.price}</p>
+              <p className="text-base sm:text-lg ">${dish.price}</p>
             </div>
           </div>
         );
       })}
-      <div className="flex flex-col sm:flex-row items-center">
-        <div className="flex justify-between w-full sm:mb-6 md:mb-8">
-          <p className="text-base sm:text-xl md:text-2xl font-semibold">
-            Production cost
-          </p>
-          <p className="text-base sm:text-xl md:text-2xl">
-            ${dishToDetails.total}
-          </p>
+      <div className="flex flex-col items-center sm:flex-col ">
+        <div className="flex justify-between w-full sm:mb-1 ">
+          <p className="text-base sm:text-lg font-semibold">Production cost</p>
+          <p className="text-base sm:text-lg">${dishToDetails.total}</p>
         </div>
-        <div className="flex justify-between w-full sm:mb-6 md:mb-8">
-          <p className="text-base sm:text-xl md:text-2xl font-semibold">
-            Cost of delivery
-          </p>
-          <p className="text-base text-black sm:text-xl md:text-2xl">$5000</p>
+        <div className="flex justify-between w-full sm:mb-2">
+          <p className="text-base sm:text-lg font-semibold">Cost of delivery</p>
+          <p className="text-base text-black sm:text-lg">$5000</p>
         </div>
         <hr className="w-full border-1 border-black" />
-        <div className="flex justify-between w-full mb-4 sm:mb-6 md:mb-8">
-          <p className="text-base sm:text-xl md:text-2xl font-semibold">Total</p>
-          <p className="text-base sm:text-xl md:text-2xl">
+        <div className="flex justify-between w-full mb-4 sm:mb-6">
+          <p className="text-base sm:text-xl font-semibold">Total</p>
+          <p className="text-base sm:text-xl">
             ${parseInt(dishToDetails.total) + 5000}
           </p>
         </div>
       </div>
-      <Navbar />
+      <div className="flex items-center justify-center">
+        <Navbar />
+      </div>
     </div>
   );
 };
